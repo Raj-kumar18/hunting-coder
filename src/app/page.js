7288d38087aaa9bbd7754e546d9fe186c69ca21a
Raj kumar <drajkumar01234@gmail.com>
2023-09-30 12:11:21 +0530

    hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client"
 import Navbar from './components/Navbar';
+import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { Client, Databases, ID } from "appwrite"
@@ -31,8 +32,37 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <h1>Welcome , to Hunting coder</h1>
+
+
+      <section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to,
+        <br className="hidden lg:inline-block"/>Hunting Coder
+      </h1>
+      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Blog</button>
+        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Contact</button>
+      </div>
+    </div>
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <Image className="object-cover object-center rounded" alt="hero" src="/hero.jpg" width="500" height="500">
+        </Image>
+    </div>
+  </div>
+</section>
+
+
+
+
+
       <div className="container mx-auto px-4 py-8">
+
+      <h1 className="title-font sm:text-4xl text-3xl mb-6 font-medium text-gray-900 text-center ">BLOGS
+      </h1>
+
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {blogPosts.map((post) => (
             <div key={post.id} className="bg-white rounded-lg shadow-md">
